b59dfc4f6bc48668532ad4c7e68354b869844050
rmoham <riyaz.ge@gmail.com>
2025-05-15 17:26:07 +0530

    Update GitHub202505151509.java

--- Projects/20250515/GitHub202505151509/src/GitHub202505151509JavaProduct/GitHub202505151509.java
+++ Projects/20250515/GitHub202505151509/src/GitHub202505151509JavaProduct/GitHub202505151509.java
@@ -5,7 +5,7 @@ public class GitHub202505151509 {
 	public static void main(String[] args) {
 		System.out.println("202505151509");
 		System.out.println("202505151509");
-				System.out.println("202505151509");
+		System.out.println("202505151509");
 		
 	}
 
