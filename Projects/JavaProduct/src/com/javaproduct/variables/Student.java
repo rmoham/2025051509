bf8fef0544160aca80e317b98a77884bfa291791
rmoham <riyaz.ge@gmail.com>
2025-05-15 23:32:59 +0530

    Update Student.java

--- Projects/JavaProduct/src/com/javaproduct/variables/Student.java
+++ Projects/JavaProduct/src/com/javaproduct/variables/Student.java
@@ -4,6 +4,7 @@ public class Student {
 
 	public static void main(String[] args) {
 		System.out.println("New Eclipse 2025");
+		System.out.println("New Eclipse 2025 fetched out from Github");
 	}
 
 }
