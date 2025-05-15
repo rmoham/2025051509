1e8e75bf32e0ea54a28f07c34a1a6d082dcc6869
riyazm <reach_riyaz@outlook.com>
2025-05-15 22:21:15 +0530

    Initial commit

---
+++ Projects/20250515/PrimitiveDataType/src/com/primitivedatatype/javalang/PrimitiveDataType.java
@@ -0,0 +1,33 @@
+package com.primitivedatatype.javalang;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		byte bYte = 10;
+		short sHort = 30;
+		char cHar = '1';
+		int inT = 20;
+		long loNg = 2222l;
+		float fLoat = 40.40f;
+		double dOuble = 20.44d;
+		
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println();
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println();
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println();
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println();
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		System.out.println();
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+	}
+
+}
